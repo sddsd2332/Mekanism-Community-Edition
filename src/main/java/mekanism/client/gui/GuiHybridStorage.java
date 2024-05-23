aef96d13d77396c6fb1cad9f21bbe6c5d90eda60
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2024-05-23 13:39:04 +0800

    TODO
    Composite storage is preliminarily completed
    [It is now possible for this block to store items, fluids, gases, and energy at the same time]
    TODO：Need to add redstone control, GUI optimization

--- src/main/java/mekanism/client/gui/GuiHybridStorage.java
+++ src/main/java/mekanism/client/gui/GuiHybridStorage.java
@@ -1,17 +1,25 @@
 package mekanism.client.gui;
 
 
+import mekanism.client.gui.element.GuiEnergyInfo;
 import mekanism.client.gui.element.GuiPlayerSlot;
 import mekanism.client.gui.element.GuiSlot;
+import mekanism.client.gui.element.gauge.GuiEnergyGauge;
+import mekanism.client.gui.element.gauge.GuiFluidGauge;
+import mekanism.client.gui.element.gauge.GuiGasGauge;
+import mekanism.client.gui.element.gauge.GuiGauge;
 import mekanism.client.gui.element.tab.GuiSecurityTab;
 import mekanism.client.gui.element.tab.GuiSideConfigurationTab;
 import mekanism.common.inventory.container.ContainerHybridStorage;
 import mekanism.common.tile.TileEntityHybridStorage;
 import mekanism.common.util.LangUtils;
+import mekanism.common.util.MekanismUtils;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Arrays;
 
 @SideOnly(Side.CLIENT)
 public class GuiHybridStorage extends GuiMekanismTile<TileEntityHybridStorage> {
@@ -24,11 +32,19 @@ public class GuiHybridStorage extends GuiMekanismTile<TileEntityHybridStorage> {
         addGuiElement(new GuiSecurityTab(this, tileEntity, resource,54,0));
         addGuiElement(new GuiPlayerSlot(this, resource, 7, 219));
         addGuiElement(new GuiSideConfigurationTab(this, tileEntity, resource));
+        addGuiElement(new GuiGasGauge(() -> tileEntity.gasTank1, GuiGauge.Type.STANDARD, this, resource, -14, 25).withColor(GuiGauge.TypeColor.BLUE));
+        addGuiElement(new GuiGasGauge(() -> tileEntity.gasTank2, GuiGauge.Type.STANDARD, this, resource, -14, 88).withColor(GuiGauge.TypeColor.ORANGE));
+        addGuiElement(new GuiFluidGauge(() -> tileEntity.fluidTank, GuiGauge.Type.STANDARD, this, resource, -14, 151).withColor(GuiGauge.TypeColor.YELLOW));
+        addGuiElement(new GuiEnergyGauge(() -> tileEntity, GuiEnergyGauge.Type.STANDARD, this, resource, -14, 214).withColor(GuiGauge.TypeColor.AQUA));
+        addGuiElement(new GuiEnergyInfo(() -> Arrays.asList(LangUtils.localize("gui.storing") + ": " + MekanismUtils.getEnergyDisplay(tileEntity.getEnergy(), tileEntity.getMaxEnergy()),
+                LangUtils.localize("gui.maxOutput") + ": " + MekanismUtils.getEnergyDisplay(tileEntity.getMaxOutput()) + "/t"), this, resource));
+
         for (int y = 0; y < 10; y++) {
             for (int x = 0; x < 12; x++) {
                 addGuiElement(new GuiSlot(GuiSlot.SlotType.NORMAL, this, resource, 7 + x * 18, 25 + y * 18));
             }
         }
+
     }
 
     @Override
--- src/main/java/mekanism/common/tile/TileEntityHybridStorage.java
+++ src/main/java/mekanism/common/tile/TileEntityHybridStorage.java
@@ -1,12 +1,16 @@
 package mekanism.common.tile;
 
+import io.netty.buffer.ByteBuf;
 import it.unimi.dsi.fastutil.booleans.BooleanArrayList;
 import mekanism.api.IConfigCardAccess;
+import mekanism.api.TileNetworkList;
+import mekanism.api.gas.*;
 import mekanism.api.transmitters.TransmissionType;
 import mekanism.common.SideData;
-import mekanism.common.base.ISideConfiguration;
+import mekanism.common.base.*;
 import mekanism.common.block.states.BlockStateMachine.MachineType;
 import mekanism.common.capabilities.Capabilities;
+import mekanism.common.config.MekanismConfig;
 import mekanism.common.integration.computer.IComputerIntegration;
 import mekanism.common.security.ISecurityTile;
 import mekanism.common.tile.component.TileComponentConfig;
@@ -14,43 +18,89 @@ import mekanism.common.tile.component.TileComponentEjector;
 import mekanism.common.tile.component.TileComponentSecurity;
 import mekanism.common.tile.component.config.DataType;
 import mekanism.common.tile.prefab.TileEntityElectricBlock;
-import mekanism.common.util.InventoryUtils;
+import mekanism.common.util.*;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Set;
 import java.util.stream.IntStream;
 
-public class TileEntityHybridStorage extends TileEntityElectricBlock implements ISideConfiguration, IComputerIntegration, ISecurityTile, IConfigCardAccess {
+public class TileEntityHybridStorage extends TileEntityElectricBlock implements ISideConfiguration, IComputerIntegration,
+        ISecurityTile, IConfigCardAccess, IGasHandler, ISustainedData, ITankManager, IFluidHandlerWrapper {
 
     private static final int[] INV_SLOTS = IntStream.range(0, 120).toArray();
     private static final boolean[] INV_SLOTS_INPUT_OUTPUT = IntStream.range(0, 120).mapToObj(it -> it >= 0).collect(BooleanArrayList::new, BooleanArrayList::add, BooleanArrayList::addAll).toBooleanArray(new boolean[0]);
-
+    private static final int GAS_OUTPUT = MekanismConfig.current().mekce.GasEjectionSpeed.val();
     public TileComponentEjector ejectorComponent;
     public TileComponentConfig configComponent;
     public TileComponentSecurity securityComponent;
+    public GasTank gasTank1 = new GasTank(10000);
+    public GasTank gasTank2 = new GasTank(10000);
+    public FluidTank fluidTank = new FluidTank(10000);
 
     public TileEntityHybridStorage() {
         super(MachineType.HYBRID_STORAGE.getBlockName(), MachineType.HYBRID_STORAGE.getStorage());
-        configComponent = new TileComponentConfig(this, TransmissionType.ITEM);
+        configComponent = new TileComponentConfig(this, TransmissionType.ITEM, TransmissionType.ENERGY,TransmissionType.FLUID, TransmissionType.GAS);
+
         configComponent.addOutput(TransmissionType.ITEM, new SideData(DataType.NONE, InventoryUtils.EMPTY));
         configComponent.addOutput(TransmissionType.ITEM, new SideData(DataType.INPUT, INV_SLOTS));
         configComponent.addOutput(TransmissionType.ITEM, new SideData(DataType.OUTPUT, INV_SLOTS));
         configComponent.addOutput(TransmissionType.ITEM, new SideData(INV_SLOTS, INV_SLOTS_INPUT_OUTPUT));
         configComponent.setConfig(TransmissionType.ITEM, new byte[]{1, 1, 1, 1, 1, 2});
 
+        configComponent.setIOConfig(TransmissionType.ENERGY);
+        configComponent.setIOConfig(TransmissionType.FLUID);
+        configComponent.getOutputs(TransmissionType.FLUID).get(2).availableSlots = new int[]{0};
 
+        configComponent.addOutput(TransmissionType.GAS, new SideData(DataType.NONE, InventoryUtils.EMPTY));
+        configComponent.addOutput(TransmissionType.GAS, new SideData(DataType.INPUT_1, new int[]{1}));
+        configComponent.addOutput(TransmissionType.GAS, new SideData(DataType.INPUT_2, new int[]{2}));
+        configComponent.addOutput(TransmissionType.GAS, new SideData(new int[]{1, 2}, new boolean[]{true, true}));
+        configComponent.addOutput(TransmissionType.GAS, new SideData(DataType.OUTPUT_1, new int[]{1}));
+        configComponent.addOutput(TransmissionType.GAS, new SideData(DataType.OUTPUT_2, new int[]{2}));
+        configComponent.setConfig(TransmissionType.GAS, new byte[]{1, 1, 1, 1, 1, 2});
 
         inventory = NonNullList.withSize(120, ItemStack.EMPTY);
         securityComponent = new TileComponentSecurity(this);
         ejectorComponent = new TileComponentEjector(this);
         ejectorComponent.setOutputData(TransmissionType.ITEM, configComponent.getOutputs(TransmissionType.ITEM).get(2));
         ejectorComponent.setInputOutputData(TransmissionType.ITEM, configComponent.getOutputs(TransmissionType.ITEM).get(3));
+        ejectorComponent.setInputOutputData(TransmissionType.GAS, configComponent.getOutputs(TransmissionType.GAS).get(3));
+        ejectorComponent.setOutputData(TransmissionType.FLUID, configComponent.getOutputs(TransmissionType.FLUID).get(2));
     }
 
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        if (!world.isRemote) {
+            if (configComponent.isEjecting(TransmissionType.ENERGY)) {
+                CableUtils.emit(this);
+            }
+            handleGasTank(gasTank1, configComponent.getSidesForData(TransmissionType.GAS, facing, 4));
+            handleGasTank(gasTank2, configComponent.getSidesForData(TransmissionType.GAS, facing, 5));
+        }
+    }
+
+    private void handleGasTank(GasTank tank, Set<EnumFacing> side) {
+        if (tank.getGas() != null) {
+            if (configComponent.isEjecting(TransmissionType.GAS)) {
+                GasStack toSend = new GasStack(tank.getGas().getGas(), tank.getStored());
+                tank.draw(GasUtils.emit(toSend, this, side), true);
+            }
+        }
+    }
 
     @Override
     public boolean isItemValidForSlot(int slotID, @Nonnull ItemStack itemstack) {
@@ -65,6 +115,31 @@ public class TileEntityHybridStorage extends TileEntityElectricBlock implements 
     @Override
     public EnumFacing getOrientation() {
         return facing;
+    }
+
+    @Override
+    public boolean sideIsConsumer(EnumFacing side) {
+        return configComponent.hasSideForData(TransmissionType.ENERGY, facing, 1, side);
+    }
+
+    @Override
+    public boolean sideIsOutput(EnumFacing side) {
+        return configComponent.hasSideForData(TransmissionType.ENERGY, facing, 2, side);
+    }
+
+    @Override
+    public double getMaxOutput() {
+        return MachineType.HYBRID_STORAGE.getStorage();
+    }
+
+    @Override
+    public double getMaxEnergy() {
+        return MachineType.HYBRID_STORAGE.getStorage();
+    }
+
+    @Override
+    public void setEnergy(double energy) {
+        super.setEnergy(energy);
     }
 
     @Override
@@ -89,8 +164,14 @@ public class TileEntityHybridStorage extends TileEntityElectricBlock implements 
 
     @Override
     public boolean isCapabilityDisabled(@Nonnull Capability<?> capability, EnumFacing side) {
-        return configComponent.isCapabilityDisabled(capability, side, facing) || super.isCapabilityDisabled(capability, side);
+        if (configComponent.isCapabilityDisabled(capability, side, facing)) {
+            return true;
+        } else if (capability == Capabilities.GAS_HANDLER_CAPABILITY  || capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return side != null && configComponent.isCapabilityDisabled(capability, side, facing);
+        }
+        return super.isCapabilityDisabled(capability, side);
     }
+
 
     @Override
     public boolean canExtractItem(int slotID, @Nonnull ItemStack itemstack, @Nonnull EnumFacing side) {
@@ -102,7 +183,8 @@ public class TileEntityHybridStorage extends TileEntityElectricBlock implements 
         if (isCapabilityDisabled(capability, side)) {
             return false;
         }
-        return capability == Capabilities.CONFIG_CARD_CAPABILITY || super.hasCapability(capability, side);
+        return capability == Capabilities.GAS_HANDLER_CAPABILITY || capability == Capabilities.CONFIG_CARD_CAPABILITY ||
+                capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY || super.hasCapability(capability, side);
     }
 
     @Override
@@ -110,8 +192,12 @@ public class TileEntityHybridStorage extends TileEntityElectricBlock implements 
         if (isCapabilityDisabled(capability, side)) {
             return null;
         }
-        if (capability == Capabilities.CONFIG_CARD_CAPABILITY) {
+        if (capability == Capabilities.GAS_HANDLER_CAPABILITY) {
+            return Capabilities.GAS_HANDLER_CAPABILITY.cast(this);
+        } else if (capability == Capabilities.CONFIG_CARD_CAPABILITY) {
             return Capabilities.CONFIG_CARD_CAPABILITY.cast(this);
+        } else if (capability == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) {
+            return CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.cast(new FluidHandlerWrapper(this, side));
         }
         return super.getCapability(capability, side);
     }
@@ -125,5 +211,162 @@ public class TileEntityHybridStorage extends TileEntityElectricBlock implements 
     @Override
     public boolean canSetFacing(@Nonnull EnumFacing facing) {
         return facing != EnumFacing.DOWN && facing != EnumFacing.UP;
+    }
+
+    @Override
+    public TileNetworkList getNetworkedData(TileNetworkList data) {
+        super.getNetworkedData(data);
+        TileUtils.addTankData(data, fluidTank);
+        TileUtils.addTankData(data, gasTank1);
+        TileUtils.addTankData(data, gasTank2);
+        return data;
+    }
+
+    @Override
+    public void handlePacketData(ByteBuf dataStream) {
+        super.handlePacketData(dataStream);
+        if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
+            TileUtils.readTankData(dataStream, fluidTank);
+            TileUtils.readTankData(dataStream, gasTank1);
+            TileUtils.readTankData(dataStream, gasTank2);
+        }
+    }
+
+    @Override
+    public void readCustomNBT(NBTTagCompound nbtTags) {
+        super.readCustomNBT(nbtTags);
+        fluidTank.readFromNBT(nbtTags.getCompoundTag("fluidTank"));
+        gasTank1.read(nbtTags.getCompoundTag("gasTank1"));
+        gasTank2.read(nbtTags.getCompoundTag("gasTank2"));
+    }
+
+
+    @Override
+    public void writeCustomNBT(NBTTagCompound nbtTags) {
+        super.writeCustomNBT(nbtTags);
+        nbtTags.setTag("fluidTank", fluidTank.writeToNBT(new NBTTagCompound()));
+        nbtTags.setTag("gasTank1", gasTank1.write(new NBTTagCompound()));
+        nbtTags.setTag("gasTank2", gasTank2.write(new NBTTagCompound()));
+    }
+
+    @Override
+    public int receiveGas(EnumFacing side, GasStack stack, boolean doTransfer) {
+        if (canReceiveGas(side, stack != null ? stack.getGas() : null)) {
+            if (configComponent.getOutput(TransmissionType.GAS, side, facing).hasSlot(1, 2)) {
+                if (stack != null) {
+                    if (gasTank1.canReceive(stack.getGas()) && gasTank2.getGasType() != stack.getGas()) {
+                        return gasTank1.receive(stack, doTransfer);
+                    }
+                    if (gasTank2.canReceive(stack.getGas()) && gasTank1.getGasType() != stack.getGas()) {
+                        return gasTank2.receive(stack, doTransfer);
+                    }
+                }
+            } else {
+                return getTank(side).receive(stack, doTransfer);
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public GasStack drawGas(EnumFacing side, int amount, boolean doTransfer) {
+        if (canDrawGas(side, null)) {
+            return getTank(side).draw(amount, doTransfer);
+        }
+        return null;
+    }
+
+    public GasTank getTank(EnumFacing side) {
+        if (configComponent.getOutput(TransmissionType.GAS, side, facing).hasSlot(1)) {
+            return gasTank1;
+        } else if (configComponent.getOutput(TransmissionType.GAS, side, facing).hasSlot(2)) {
+            return gasTank2;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean canReceiveGas(EnumFacing side, Gas type) {
+        if (configComponent.getOutput(TransmissionType.GAS, side, facing).hasSlot(1, 2)) {
+            return gasTank1.canReceive(type) || gasTank2.canReceive(type);
+        }
+        return getTank(side) != null && getTank(side).canReceive(type);
+    }
+
+    @Override
+    public boolean canDrawGas(EnumFacing side, Gas type) {
+        return getTank(side) != null && getTank(side).getGas() != null && getTank(side).stored.getGas() == type;
+    }
+
+
+    @Override
+    public void writeSustainedData(ItemStack itemStack) {
+        if (fluidTank.getFluid() != null) {
+            ItemDataUtils.setCompound(itemStack, "fluidTank", fluidTank.getFluid().writeToNBT(new NBTTagCompound()));
+        }
+        if (gasTank1.getGas() != null) {
+            ItemDataUtils.setCompound(itemStack, "gasTank1", gasTank1.getGas().write(new NBTTagCompound()));
+        }
+        if (gasTank2.getGas() != null) {
+            ItemDataUtils.setCompound(itemStack, "gasTank2", gasTank2.getGas().write(new NBTTagCompound()));
+        }
+    }
+
+    @Override
+    public void readSustainedData(ItemStack itemStack) {
+        fluidTank.setFluid(FluidStack.loadFluidStackFromNBT(ItemDataUtils.getCompound(itemStack, "fluidTank")));
+        gasTank1.setGas(GasStack.readFromNBT(ItemDataUtils.getCompound(itemStack, "gasTank1")));
+        gasTank2.setGas(GasStack.readFromNBT(ItemDataUtils.getCompound(itemStack, "gasTank2")));
+    }
+
+    @Nonnull
+    @Override
+    public GasTankInfo[] getTankInfo() {
+        return new GasTankInfo[]{gasTank1, gasTank2};
+    }
+
+    @Override
+    public Object[] getTanks() {
+        return new Object[]{fluidTank, gasTank1, gasTank2};
+    }
+
+    @Override
+    public int fill(EnumFacing from, @Nonnull FluidStack resource, boolean doFill) {
+        return fluidTank.fill(resource, doFill);
+    }
+
+    @Override
+    @Nullable
+    public FluidStack drain(EnumFacing from, int maxDrain, boolean doDrain) {
+        return fluidTank.drain(maxDrain, doDrain);
+    }
+
+    @Override
+    public boolean canFill(EnumFacing from, @Nonnull FluidStack fluid) {
+        if (configComponent.getOutput(TransmissionType.FLUID, from, facing).ioState == SideData.IOState.INPUT) {
+            return FluidContainerUtils.canFill(fluidTank.getFluid(), fluid);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean canDrain(EnumFacing from, @Nullable FluidStack fluid) {
+        if (configComponent.getOutput(TransmissionType.FLUID, from, facing).ioState == SideData.IOState.OUTPUT) {
+            return FluidContainerUtils.canDrain(fluidTank.getFluid(), fluid);
+        }
+        return false;
+    }
+
+    @Override
+    public FluidTankInfo[] getTankInfo(EnumFacing from) {
+        if (configComponent.getOutput(TransmissionType.FLUID, from, facing).ioState != SideData.IOState.OFF) {
+            return new FluidTankInfo[]{fluidTank.getInfo()};
+        }
+        return PipeUtils.EMPTY;
+    }
+
+    @Override
+    public FluidTankInfo[] getAllTanks() {
+        return new FluidTankInfo[]{fluidTank.getInfo()};
     }
 }
