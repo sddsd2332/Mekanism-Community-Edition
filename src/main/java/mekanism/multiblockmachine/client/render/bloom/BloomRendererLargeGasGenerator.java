bf3435d9914034915ceff5d1c50b5f1f7bc85c3b
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2024-05-31 11:56:54 +0800

    Adjust the position of the glow texture
    Glow rendering is now done correctly

--- src/main/java/mekanism/multiblockmachine/client/render/bloom/BloomRendererLargeGasGenerator.java
+++ src/main/java/mekanism/multiblockmachine/client/render/bloom/BloomRendererLargeGasGenerator.java
@@ -4,6 +4,7 @@ import gregtech.client.renderer.IRenderSetup;
 import gregtech.client.utils.EffectRenderContext;
 import gregtech.client.utils.IBloomEffect;
 import mekanism.api.util.time.Timeticks;
+import mekanism.client.render.MekanismRenderer;
 import mekanism.multiblockmachine.client.model.generator.ModelLargeGasGenerator;
 import mekanism.multiblockmachine.client.render.generator.RenderLargeGasGenerator;
 import mekanism.multiblockmachine.common.tile.generator.TileEntityLargeGasGenerator;
@@ -36,22 +37,20 @@ public class BloomRendererLargeGasGenerator implements IBloomEffect, IRenderSetu
     public void renderBloomEffect(@NotNull final BufferBuilder bufferBuilder, @NotNull final EffectRenderContext effectRenderContext) {
         GlStateManager.pushMatrix();
         BlockPos pos = gasGenerator.getPos();
-
         int x = pos.getX();
         int y = pos.getY();
         int z = pos.getZ();
         double cX = effectRenderContext.cameraX();
         double cY = effectRenderContext.cameraY();
         double cZ = effectRenderContext.cameraZ();
-
         GlStateManager.translate((x + 0.5F) - cX, (y + 1.5F) - cY, (z + 0.5F) - cZ);
         RenderLargeGasGenerator renderer = (RenderLargeGasGenerator) TileEntityRendererDispatcher.instance.renderers.get(TileEntityLargeGasGenerator.class);
+        MekanismRenderer.rotate(gasGenerator.facing, 0, 180, 90, 270);
+        GlStateManager.rotate(180, 0, 0, 1);
         Timeticks time = renderer.getTime();
         ModelLargeGasGenerator model = renderer.getModel();
         double tick = time.getValue() / 20F;
-
-        model.render(tick, 0.0625F, gasGenerator.getActive(), Minecraft.getMinecraft().renderEngine);
-
+        model.renderBloom(tick, 0.0625F, gasGenerator.getActive(), Minecraft.getMinecraft().renderEngine);
         GlStateManager.popMatrix();
     }
 
