a107294f5c27d501f8b307323f788ca61fad8343
sddsd2332（ARrain乐雪） <3207168480@qq.com>
2024-06-18 17:26:28 +0800

    Add a block overlay

--- src/main/java/mekanism/common/tile/TileEntityModificationStation.java
+++ src/main/java/mekanism/common/tile/TileEntityModificationStation.java
@@ -1,13 +1,16 @@
 package mekanism.common.tile;
 
+import mekanism.api.Coord4D;
+import mekanism.common.base.IBoundingBlock;
 import mekanism.common.block.states.BlockStateMachine.*;
 import mekanism.common.tile.prefab.TileEntityOperationalMachine;
+import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.NonNullListSynchronized;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import org.jetbrains.annotations.NotNull;
 
-public class TileEntityModificationStation extends TileEntityOperationalMachine {
+public class TileEntityModificationStation extends TileEntityOperationalMachine implements IBoundingBlock {
 
     public TileEntityModificationStation() {
         super("null", MachineType.MODIFICATION_STATION, 0, 40);
@@ -39,5 +42,22 @@ public class TileEntityModificationStation extends TileEntityOperationalMachine 
     @Override
     public int[] getSlotsForFace(@NotNull EnumFacing side) {
         return new int[0];
+    }
+
+    @Override
+    public void onPlace() {
+        EnumFacing right = MekanismUtils.getRight(facing);
+        MekanismUtils.makeBoundingBlock(world, getPos().up(), Coord4D.get(this));
+        MekanismUtils.makeBoundingBlock(world, getPos().offset(right), Coord4D.get(this));
+        MekanismUtils.makeBoundingBlock(world, getPos().offset(right).up(), Coord4D.get(this));
+    }
+
+    @Override
+    public void onBreak() {
+        EnumFacing right = MekanismUtils.getRight(facing);
+        world.setBlockToAir(getPos().offset(right).up());
+        world.setBlockToAir(getPos().offset(right));
+        world.setBlockToAir(getPos().up());
+        world.setBlockToAir(getPos());
     }
 }
